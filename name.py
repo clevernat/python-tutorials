af08e0dc45f93d4c3b9aa561e33459fd03e84ae5
clevernat <clevernat1@gmail.com>
2022-04-05 09:10:24 +0000

    Initial Commit

---
+++ name.py
@@ -0,0 +1,190 @@
+
+#############################################################################################################
+
+# python programming
+
+
+'''
+
+Stastically typed programming language - Fortran(low level) - Fast
+Dynamically typed programming language - Python(high level) - Slower
+
+Python is a dynamic, multipurpose programming language...
+
+Multipurpose programming language - Data Science and Analysis, Machine Learning, Artificial Intelligence, Software Engineering, Automation, etc...
+
+Platforms - Anaconda, Miniconda, Visual Studio Code, Pycharm, Sublime Text, Atom, etc...
+
+
+# Basics 
+
+
+Topics to be treated
+
+# Basics
+-------------------
+# Variables
+# Data Types
+# Operations
+# Type conversion
+# Data Structures
+# Control Flows
+# Packages and Modules
+# Classes*
+
+# where to get help
+1. Follow the code
+2. Google
+3. Stack Overflow
+
+
+# Variables
+Variables - are containers that store values.
+
+# Syntax
+variable_name = value
+
+eg. 
+name = 'Benjamin'
+school = 'KNUST'
+level = 400
+first_name = 'Nathaniel'
+
+Jeff Solomon
+
+# rules to follow in variable
+1. Does not start with capital letter. eg. Daniel, Name, School...instead, daniel, name, school
+2. combined names are separated with underscore. eg. jeff_solomon, first_name, last_name
+3. does not start with special characters. eg. _name, .school, ;solomon
+4. variable name should be descriptive. eg. first_name = 'Solomon'
+
+# DATA TYPES
+we have primitive data type and reference data type
+
+# Primitive Data Type
+string - '' or "". eg. name = 'Oppong' or name = "Oppong"
+boolean - True or False - eg. eaten = True, no_school = False
+numbers - integer and float 
+
+eg. of integers = 3, 5, 5065, etc...
+eg. of float = 4.5, -55.4, etc...
+
+boolean - True or False
+
+Example
+name_of_person = True;
+
+is_good = False
+
+
+# Operations
+Addition --- +
+Subtraction --- -
+Multiplication --- *
+Division --- / or //
+Modulus -- %
+
+eg. print(2 + 2)
+print(4 / 3)
+print(5 * 7)
+
+# Type conversion
+from interger to float
+from interger to string
+from string to integer
+
+
+
+# Day 2
+
+# Escape sequence
+eg.
+name = 'Nathaniel \nSamuel'
+name = 'Nathaniel \n Samuel'
+name = "Nathaniel 'Samuel'"
+name = 'Nathaniel \'Samuel\''
+name = "Nathaniel  \"Samuel\""
+name = "Nathaniel \\ Samuel"
+name = "Nathaniel \\\\ Samuel"
+
+
+
+##### working with strings
+name = 'Nathaniel'
+
+# to know the len of the string - len()
+
+print(len(name))
+
+### string slicing
+name_of_variable[::]
+
+before colon - start 
+in-btn - end 
+last - step
+
+name = 'Nathaniel'
+numbers = '123456789'
+
+print(name[:])
+print(name[::])
+print(name[0])
+print(name[1])
+print(name[-1])
+print(name[0:4])
+print(numbers[0::2])
+
+'''
+
+# print()
+# input()
+
+
+
+# full_name = input("What's your name: ")
+# print(full_name)
+# print(type(full_name))
+
+# print()
+# type()
+# input()
+# float()
+
+
+
+
+a = 4
+b = 'Nar'
+c = True
+d = 34.6
+
+# print(type(a))
+# print(float(a))
+# print(int(d))
+
+# print(float(c))
+
+# print(type(c))
+# print(float(c))
+
+
+# full_name = input('Enter your full name: ')
+# print(full_name)
+
+
+# primitive data type (string)
+# name = 'Nathaniel \n Samuel'
+
+# name = "Nathaniel Samuel"
+name = "Nathaniel 'Samuel'"
+name = 'Nathaniel \'Samuel\''
+name = "Nathaniel  \"Samuel\""
+name = "Nathaniel \\ Samuel"
+name = "Nathaniel \\\\ Samuel"
+# print(name)
+
+# working with strings
+name = 'Nathaniel'
+
+# to know the len of the string - len()
+
